544a6f1f13deb0110b20cb35b8cea88a1281ba65
Titch Deane <titch@hotmail.co.uk>
2018-02-21 10:08:34 -0800

    Authorize KeleClient

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,8 +1,13 @@
+require 'httparty'
 
+class Kele
+  include HTTParty
 
   def initialize(email, password)
     @email = email
     @password = password
-
-
+    @bloc_api = 'https://www.bloc.io/api/v1'
+    @auth_token = self.class.post("#{@bloc_api}"'/sessions', query: { email: @email, password: @password })
   end
+
+end
