7384b0e11a683e1b091a010f350a9953eda8181e
Titch Deane <titch@hotmail.co.uk>
2018-02-21 13:19:52 -0800

    Implement get messages & create messages

--- lib/kele.rb
+++ lib/kele.rb
@@ -10,7 +10,7 @@ class Kele
     @email = email
     @password = password
     @bloc_api = 'https://www.bloc.io/api/v1'
-    response = self.class.post("#{@bloc_api}"'/sessions', query: { email: @email, password: @password })
+    response = self.class.post("#{@bloc_api}"'/sessions', body: { email: @email, password: @password })
     @auth_token = response["auth_token"]
   end
 
@@ -23,6 +23,28 @@ class Kele
     response = self.class.get("#{@bloc_api}" + "/mentors/" + "#{mentor_id}" + "/student_availability", headers: { "authorization" => @auth_token })
     JSON.parse(response.body)
   end
+
+  def get_messages(page = nil)
+    if page == nil
+      response = self.class.get("#{@bloc_api}" + "/message_threads", headers: { "authorization" => @auth_token })
+    else
+      response = self.class.get("#{@bloc_api}" + "/message_threads", body: { "page": page }, headers: { "authorization" => @auth_token })
+    end
+    JSON.parse(response.body)
+  end
+
+  def create_message(recipient_id, messagebody, subject = nil, token = nil)
+    response = self.class.post("#{@bloc_api}" + "/messages",
+      body: {
+        "sender": @email,
+        "recipient_id": recipient_id,
+        "token": token,
+        "subject": subject,
+        "stripped-text": messagebody
+      },
+      headers: { "authorization" => @auth_token })
+    end
+
 
 
 end
