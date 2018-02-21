41e3e586de0dbaf084c2579f3d2b0483f07f4f9d
Titch Deane <titch@hotmail.co.uk>
2018-02-21 11:08:28 -0800

    Get Mentor Availability

--- lib/kele.rb
+++ lib/kele.rb
@@ -17,4 +17,9 @@ class Kele
     JSON.parse(response.body)
   end
 
+  def get_mentor_availability(mentor_id)
+    response = self.class.get("#{@bloc_api}" + "/mentors/" + "#{mentor_id}" + "/student_availability", headers: { "authorization" => @auth_token })
+    JSON.parse(response.body)
+  end
+
 end
