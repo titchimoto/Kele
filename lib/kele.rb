57960aa0076451586cdb3840446c1f986f612e2d
Titch Deane <titch@hotmail.co.uk>
2018-02-21 13:37:00 -0800

    Implement create submission

--- lib/kele.rb
+++ lib/kele.rb
@@ -45,6 +45,20 @@ class Kele
       headers: { "authorization" => @auth_token })
     end
 
+    def create_submission(checkpoint_id, enrollment_id, comment = nil, assignment_branch = nil, assignment_commit_link = nil)
+      response = self.class.post("#{@bloc_api}" + "/checkpoint_submissions",
+        body: {
+          "checkpoint_id": checkpoint_id,
+          "enrollment_id": enrollment_id,
+          "assignment_branch": assignment_branch,
+          "assignment_commit_link": assignment_commit_link,
+          "comment": comment
+        },
+        headers: { "authorization" => @auth_token })
+
+
+    end
+
 
 
 end
